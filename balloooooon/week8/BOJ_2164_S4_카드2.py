6b7671f9f1787aa71b65c43e2ac7c2969d49a39a
jungwoo <qkdqkd7418@naver.com>
2024-02-28 01:10:30 +0900

    BOJ 2164 S4 카드2

---
+++ balloooooon/week8/BOJ_2164_S4_카드2.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+queue = deque()
+
+N = int(input())
+
+for i in range(1,N+1):
+    queue.append(i)
+
+while len(queue) != 1:
+    queue.popleft()
+    queue.append(queue.popleft())
+print(queue[0])
