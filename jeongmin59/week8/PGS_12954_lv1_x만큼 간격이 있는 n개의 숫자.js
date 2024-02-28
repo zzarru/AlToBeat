367c0c9aeee748de11903ed37e8d27973d076746
jeongmingong <jmgong59@gmail.com>
2024-02-28 18:37:00 +0900

    PGS 12954 lv1 x만큼 간격이 있는 n개의 숫자

---
+++ jeongmin59/week8/PGS_12954_lv1_x만큼 간격이 있는 n개의 숫자.js
@@ -0,0 +1,18 @@
+// lv1_x만큼 간격이 있는 n개의 숫자
+function solution(x, n) {
+  var answer = [];
+
+  for (i = 1; i < n + 1; i++) {
+    num = x * i;
+    answer.push(num);
+  }
+
+  return answer;
+}
+
+// 다른 사람 풀이
+function solution(x, n) {
+  return Array(n)
+    .fill(x)
+    .map((v, i) => (i + 1) * v);
+}
