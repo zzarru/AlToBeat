cbc63d08d3f5b40dc91d209c2f5ca73b9cccbe83
Bang Jung Woo <122415795+balloooooon@users.noreply.github.com>
2024-03-12 19:05:24 +0900

    Add files via upload

--- balloooooon/week10/BOJ_15649_S3_N과 M.py
+++ balloooooon/week10/BOJ_15649_S3_N과 M.py
@@ -1,23 +1,15 @@
-while 1:
-    num_list = list(map(int,input().split()))
-    if num_list[0]:
-        n = num_list.pop(0)
-        temp = []
-        num_list.sort()
-        visited = [0 for _ in range(n)]
-        def lotto(lv,start):
-            if lv == 6:
-                print(*temp)
-                return
-            for i in range(start,n):
-                if not visited[i] and start-1 < i:
-                    temp.append(num_list[i])
-                    visited[i] = True
-                    lotto(lv+1,i + 1)
-                    visited[i] = False
-                    temp.pop()
-
-        lotto(0,0)
-        print()
+n,m=map(int,input().split())
+visited=[0]*(n+1)
+stack=[]
+def per(a,m):
+    if a==m:
+        print(*stack)
     else:
-        break
+        for i in range(1,n+1):
+            if not visited[i]:
+                visited[i]=True
+                stack.append(i)
+                per(a+1,m)
+                visited[i]=False
+                stack.pop()
+per(0,m)
