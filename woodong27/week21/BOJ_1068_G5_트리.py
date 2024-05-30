010938559acc9fda4bd1b8e6b7bc33d766700923
woodong27 <cdw5548@gmail.com>
2024-05-30 21:12:29 +0900

    BOJ 1068 G5 트리

---
+++ woodong27/week21/BOJ_1068_G5_트리.py
@@ -0,0 +1,43 @@
+# BOJ 1068 G5 트리
+
+"""
+삭제하고자 하는 노드를 tree의 node 목록에서 지우고 dfs를 실행하면 된다.
+탐색 중 자식노드가 없는 노드를 만나면 리프노드이므로 answer에 1을 더해준다.
+"""
+
+import sys
+
+
+def dfs(start):
+    global answer
+    if not tree[start]:
+        answer += 1
+        return
+    for node in tree[start]:
+        dfs(node)
+
+
+if __name__ == '__main__':
+    enter = sys.stdin.readline
+    n = int(enter())
+    parents = list(map(int, enter().split()))
+
+    tree = [[] for _ in range(n)]
+    root = 0
+    for i in range(n):
+        if parents[i] == -1:
+            root = i
+        else:
+            tree[parents[i]].append(i)
+
+    delete = int(enter())
+    answer = 0
+    if delete == root:
+        sys.stdout.write(f'{answer}\n')
+    else:
+        for i in range(n):
+            if delete in tree[i]:
+                tree[i].remove(delete)
+
+        dfs(root)
+        sys.stdout.write(f'{answer}\n')
